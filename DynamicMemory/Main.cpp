bb940d540c8d5c78a8e86eafd7263ec296ebe3d9
ArtHoly26 <112950470+ArtHoly26@users.noreply.github.com>
2023-04-01 10:35:37 +0200

    update

--- DynamicMemory/Main.cpp
+++ DynamicMemory/Main.cpp
@@ -13,28 +13,30 @@ void Print(int* const arr, int const size)
 	for (int i = 0; i < size; i++) cout << arr[i]<<"\t";
 }
 
-void AddElement(int *& arr, int &size, int const endElement)
+void Push_back(int *&arr, int &size, int const value)
 {
 	int* NewArray = new int[size + 1];
 	for (int i = 0; i < size; i++) NewArray[i] = arr[i];
-
-	NewArray[size] = endElement;
-	size++;
 	delete[] arr;
 	arr = NewArray;
+	NewArray[size] = value;
+	size++;
 }
 
 int main()
 {
 	setlocale(LC_ALL, "Rus");
 	int size;
+	int value;
 	cout << "Введите размер массива:" << endl;
 	cin >> size;
+	cout << "Введите число которое необходимо передать в конец массива:" << endl;
+	cin >> value;
 	int* arr = new int[size];
 	FillRand(arr,size);
 	Print(arr, size);
 	cout << endl;
-	AddElement(arr, size, 101);
+	Push_back(arr, size, value);
 	Print(arr, size);
 	delete[] arr;
 }
---
+++ TranferParametrs/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void Exchange(int a, int b);
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a = 2, b = 3;
+	cout << a << "\t" << b << endl;
+	Exchange(a, b);
+}
+
+void Exchange(int a, int b)
+{
+	int buffer = a;
+	a = b;
+	b = buffer;
+}
